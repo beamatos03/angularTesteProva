8004254131486162c66468b30e6ae96dba7d768e
beamatos03 <bea.matos1978@gmail.com>
2023-06-13 08:47:04 -0300

    a

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AnunciosComponent } from './anuncios/anuncios.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AnunciosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
